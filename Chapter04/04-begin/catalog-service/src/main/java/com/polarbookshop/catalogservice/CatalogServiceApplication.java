686bcd017c5c75bfccea4b28ec1cc201032f90d4
lvm3632 <lvm3632@gmail.com>
2025-01-06 18:45:57 -0600

    Polar environment variables

--- Chapter04/04-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ Chapter04/04-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,35 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class CatalogServiceApplication {
+@ConfigurationPropertiesScan
+public class CatalogServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private PolarProperties polar;
+
+	@Value("${server.port}")
+	private String serverPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(env.getProperty("server.port"));
+		System.out.println(serverPort);
+		System.out.println(polar.getGreeting());
+	}
 }
