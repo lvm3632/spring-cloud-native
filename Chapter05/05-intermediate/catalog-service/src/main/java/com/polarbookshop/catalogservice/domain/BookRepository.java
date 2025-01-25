719e3be02851a3e7d8883cbe72b52c28f8c1cc6c
lvm3632 <lvm3632@gmail.com>
2025-01-25 12:32:53 -0600

    Testcontainers and flyweight

--- Chapter05/05-intermediate/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ Chapter05/05-intermediate/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -7,14 +7,17 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface BookRepository extends CrudRepository<Book,Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
 	Optional<Book> findByIsbn(String isbn);
+
 	boolean existsByIsbn(String isbn);
 
 	@Modifying
 	@Transactional
 	@Query("delete from Book where isbn = :isbn")
 	void deleteByIsbn(String isbn);
+
+	// https://spring.io/projects/spring-data
 
 }
